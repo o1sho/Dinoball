c6d3e07b14f7e422d205cde88b604ec8fd342bc8
o1sho <vityakobzev1234@gmail.com>
2023-08-31 16:39:04 +0700

    ver 0.4

---
+++ Assets/Scripts/InternationalText/Language.cs
@@ -0,0 +1,29 @@
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class Language : MonoBehaviour
+{
+    public static Language instance { get; private set; }
+
+    public string currentLanguage;
+
+    [DllImport("__Internal")]
+    private static extern string GetLang();
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+
+#if !UNITY_EDITOR && UNITY_WEBGL
+            currentLanguage = GetLang();
+#endif
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
